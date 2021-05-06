e7a60b34c33d8c3a06caebbec5a78610cefd2144
Jacob Thomas <jcthomas00@gmail.com>
2021-05-06 16:11:12 -0500

    Added cors package to allow CORS requests on Heroku

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use('/users',usersRoutes);
 app.get('/', (request,response)=>{
-    console.log(`[test]`);
-    response.send("Hola Cola!");
+    response.send("Contact Jacob.Thomas@outlook.com for more information");
 
 })
 app.listen(PORT, () => {console.log(`Server running on Port that is: ${PORT}`)});
